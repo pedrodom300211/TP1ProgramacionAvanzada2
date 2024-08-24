37684ce72056e38221950fe2037e8da5ffc50d5a
pedrodom300211 <pedrodinguez2003@gmail.com>
2024-08-23 21:02:31 -0300

    Hice suma y borrar

--- app/src/main/java/com/example/tp_1/Ejercicio2.kt
+++ app/src/main/java/com/example/tp_1/Ejercicio2.kt
@@ -5,8 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+import android.view.View
+import android.util.Log
 
 class Ejercicio2 : AppCompatActivity() {
+    lateinit var tv_num: TextView
+
+    var Primervalor: Double = 0.0
+    var Segundovalor: Double = 0.0
+    var res :Double= 0.0
+
+
+    ///operacion 0=nada 1=suma 2=multiplicacion 3=dividir 4=resta
+    var operacion: Int = 0
+    var TextoPantalla: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +31,115 @@ class Ejercicio2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btn_Igual: Button = findViewById(R.id.btn_igual)
+        val btn_borrar: Button = findViewById(R.id.btn_borrar)
+        tv_num = findViewById(R.id.textView4)
+
+
+        btn_Igual.setOnClickListener{
+            Segundovalor= tv_num.text.toString().toDouble()
+            Log.d("TAG", "Valor Primer numero: " + Primervalor);
+            Log.d("TAG", "Valor Operador: " + operacion);
+            Log.d("TAG", "Valor segundo numero: " + Segundovalor);
+            when(operacion){
+                1-> res = Primervalor + Segundovalor
+                /*2-> res = numero1- num2
+                3-> res = numero1 * num2
+                4-> res = numero1 / num2*/
+            }
+            tv_num.setText(res.toString())
+
+            operacion=0
+        }
+        btn_borrar.setOnClickListener{
+            tv_num.setText("")
+
+            operacion = 0
+        }
+
     }
+
+    fun clickNumero(view: View) {
+
+        var num = tv_num.text.toString()
+
+        when (view.id ) {
+            R.id.btn_0 -> {
+                if(operacion==0)
+                tv_num.setText(num + "0")
+
+
+            }
+            R.id.btn_1 -> {
+                    tv_num.setText(num + "1")
+            }
+            R.id.btn_2 -> {
+
+                    tv_num.setText(num + "2")
+
+            }
+            R.id.btn_3 ->{
+
+                    tv_num.setText(num + "3")
+
+            }
+            R.id.btn_4 -> {
+
+                    tv_num.setText(num + "4")
+
+            }
+            R.id.btn_5 -> {
+
+                    tv_num.setText(num + "5")
+
+            }
+            R.id.btn_6 -> {
+
+                    tv_num.setText(num + "6")
+
+            }
+            R.id.btn_7 -> {
+
+                    tv_num.setText(num + "7")
+
+
+            }
+            R.id.btn_8 ->{
+
+                    tv_num.setText(num + "8")
+
+            }
+            R.id.btn_9 -> {
+
+                    tv_num.setText(num + "9")
+
+            }
+
+        }
+
+    }
+
+    fun clickOperacion(view: View) {
+        Primervalor = tv_num.text.toString().toDouble()
+        TextoPantalla = tv_num.text.toString()
+        when (view.id) {
+            R.id.btn_mas -> {
+                tv_num.setText("")
+                operacion = 1
+            }
+            /* R.id.btnRestar ->{
+                tv_num1.setText(num2+"-")
+                oper = 2
+            }
+            R.id.btnMult ->{
+                tv_num1.setText(num2+"*")
+                oper = 3
+            }
+            R.id.btnDividir -> {
+                tv_num1.setText(num2+"/")
+                oper = 4
+            }*/
+        }
+    }
+
 }
